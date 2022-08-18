543b56fca29091ed1272edd3635d44e91437f355
Alexeybmw <109792704+Alexeybmw@users.noreply.github.com>
2022-08-18 11:23:17 +0300

    Возводит в степень

---
+++ zadacha_25/Program.cs
@@ -0,0 +1,17 @@
+﻿int Exp (int A,int B)
+{
+      int result = 1;
+      for (int i=1;i<=B;i++)
+      {
+            result = result * A;
+      }
+      
+      return result;
+}
+Console.Write("Введите число A; ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B; ");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+int exp = Exp(numA,numB);
+Console.WriteLine("->" + exp);
