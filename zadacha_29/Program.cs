704d9fbe0996a4913c2f99a9be1334a3bf6254a7
Alexeybmw <109792704+Alexeybmw@users.noreply.github.com>
2022-08-20 10:00:15 +0300

    Вывели числа в массив

---
+++ zadacha_29/Program.cs
@@ -0,0 +1,27 @@
+﻿class Program
+{
+
+      static void Main (string[] args)
+      {
+            string number = Console.ReadLine();
+            string[]array = number.Split( ',');
+            Console.Write("[ ");
+            for(int i = 0;i < array.Length-1;i++)
+            {
+                  Console.Write(array[i]+ ", ");
+            }
+            Console.Write(array[array.Length-1]+ " ]");
+      }
+
+
+
+
+
+
+
+
+
+
+
+
+}
